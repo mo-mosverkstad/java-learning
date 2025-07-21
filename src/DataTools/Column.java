9850a78661bfad965d7c10a0bc59741c98533bb5
mo-mosverkstad <mo.mosverkstad@gmail.com>
2025-07-21 10:42:09 +0200

    Update Column.java
    Update Column.java

--- src/DataTools/Column.java
+++ src/DataTools/Column.java
@@ -2,7 +2,7 @@ package DataTools;
 
 interface Column {
     public void append(Object value);
-    public void set(Object value);
+    public void set(int index, Object value);
     public Object get(int index);
     public int size();
 }
