0898f409e0ddc45313a0840bfcb62f4080f21ff5
mo-mosverkstad <mo.mosverkstad@gmail.com>
2026-05-01 13:58:18 +0200

    Merge half written report

--- maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DatabaseHandler.java
+++ maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DatabaseHandler.java
@@ -2,22 +2,27 @@ package se.ebikerepair.data;
 
 import java.util.List;
 
+// !!! Javadoc not written !!!
 public class DatabaseHandler implements DataHandler {
     private String databaseName;
 
+    // !!! Javadoc not written !!!
     public void initDataHandler(String resourceName){
         databaseName = resourceName;
         throw new NoDatabaseException(databaseName);
     }
 
+    // !!! Javadoc not written !!!
     public boolean isInitialized(){
         return false;
     }
 
+    // !!! Javadoc not written !!!
     public <T> List<T> readList(Class<T> clazz){
         throw new NoDatabaseException(databaseName);
     }
 
+    // !!! Javadoc not written !!!
     public <T> void writeList(List<T> list){
         throw new NoDatabaseException(databaseName);
     }
