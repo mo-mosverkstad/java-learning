074ccbaf85bc214911ef19b62586337640d446a2
mo-mosverkstad <mo.mosverkstad@gmail.com>
2026-05-01 14:52:28 +0200

    Half written, relevant code added on report

--- maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DataHandlerFactory.java
+++ maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DataHandlerFactory.java
@@ -2,10 +2,24 @@ package se.ebikerepair.data;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Factory for creating {@link DataHandler} instances by class name using reflection.
+ */
 public class DataHandlerFactory {
 
     private DataHandlerFactory() {}
-    
+
+    /**
+     * Creates a DataHandler instance from the given fully qualified class name.
+     *
+     * @param className the fully qualified class name of the DataHandler implementation
+     * @return a new DataHandler instance
+     * @throws ClassNotFoundException if the class cannot be found
+     * @throws NoSuchMethodException if the class has no no-arg constructor
+     * @throws InstantiationException if the class cannot be instantiated
+     * @throws IllegalAccessException if the constructor is not accessible
+     * @throws InvocationTargetException if the constructor throws an exception
+     */
     public static DataHandler create(String className) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         Class<?> classObject = Class.forName(className);
         return (DataHandler) classObject.getConstructor().newInstance();
--- maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DatabaseHandler.java
+++ maven-projects/ElectricBikeRepair/src/main/java/se/ebikerepair/data/DatabaseHandler.java
@@ -2,27 +2,52 @@ package se.ebikerepair.data;
 
 import java.util.List;
 
-// !!! Javadoc not written !!!
+/**
+ * A data handler that simulates a database connection.
+ * All operations throw {@link NoDatabaseException} since no actual database is available.
+ */
 public class DatabaseHandler implements DataHandler {
     private String databaseName;
 
-    // !!! Javadoc not written !!!
+    /**
+     * Attempts to initialize the database connection.
+     *
+     * @param resourceName the name of the database resource
+     * @throws NoDatabaseException always, since no database is available
+     */
     public void initDataHandler(String resourceName){
         databaseName = resourceName;
         throw new NoDatabaseException(databaseName);
     }
 
-    // !!! Javadoc not written !!!
+    /**
+     * Returns whether the database handler is initialized.
+     *
+     * @return always {@code false} since no database is available
+     */
     public boolean isInitialized(){
         return false;
     }
 
-    // !!! Javadoc not written !!!
+    /**
+     * Reads a list of objects from the database.
+     *
+     * @param <T> the type of objects to read
+     * @param clazz the class of the objects to deserialize
+     * @return never returns normally
+     * @throws NoDatabaseException always, since no database is available
+     */
     public <T> List<T> readList(Class<T> clazz){
         throw new NoDatabaseException(databaseName);
     }
 
-    // !!! Javadoc not written !!!
+    /**
+     * Writes a list of objects to the database.
+     *
+     * @param <T> the type of objects to write
+     * @param list the list of objects to persist
+     * @throws NoDatabaseException always, since no database is available
+     */
     public <T> void writeList(List<T> list){
         throw new NoDatabaseException(databaseName);
     }
